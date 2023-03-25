8e55ec65816f13eb96a57aa6d613b0e8b109eb46
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-25 20:02:51 +0200

    Added image resizer

--- src/main/java/com/example/hoi4modder/model/Resizer.java
+++ src/main/java/com/example/hoi4modder/model/Resizer.java
@@ -2,11 +2,20 @@ package com.example.hoi4modder.model;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 public class Resizer {
-    BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
+    private BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
         BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics2D = resizedImage.createGraphics();
         graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
         graphics2D.dispose();
         return resizedImage;
+    }
+    public BufferedImage resizeToSmall(BufferedImage origin) {
+        return resizeImage(origin, 10, 7);
+    }
+    public BufferedImage resizeToMedium(BufferedImage origin) {
+        return resizeImage(origin, 41, 26);
+    }
+    public BufferedImage resizeToBig(BufferedImage origin) {
+        return resizeImage(origin, 82, 52);
     }
 }
