357c31a4be3aeaa0b2dc2844a8ff15da0670c334
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-26 18:50:15 +0300

    Implemented lazy print style

--- src/main/java/com/example/hoi4modder/model/files/properties/printstyles/LazyPrintStyle.java
+++ src/main/java/com/example/hoi4modder/model/files/properties/printstyles/LazyPrintStyle.java
@@ -4,11 +4,13 @@ import com.example.hoi4modder.model.files.properties.SavedElement;
 
 public class LazyPrintStyle implements PrintStyle{
     @Override
-    public String printStyled(ListElement element) {
-        StringBuilder builder = new StringBuilder();
-        for(;;) {
-
+    public String printStyled(ListElement list) {
+        StringBuilder builder = new StringBuilder(list.prefix());
+        for(SavedElement savedElement : list.getElements()) {
+            builder.append(savedElement.toFile());
         }
+        builder.append(list.suffix());
+        return builder.toString();
     }
 
     @Override
--- src/main/java/com/example/hoi4modder/model/files/properties/printstyles/ListElement.java
+++ src/main/java/com/example/hoi4modder/model/files/properties/printstyles/ListElement.java
@@ -1,6 +1,13 @@
 package com.example.hoi4modder.model.files.properties.printstyles;
 
+import com.example.hoi4modder.model.files.properties.Property;
 import com.example.hoi4modder.model.files.properties.SavedElement;
 
+import java.util.List;
+
 public interface ListElement extends SavedElement {
+    String prefix();
+    String suffix();
+
+    List<SavedElement> getElements();
 }
