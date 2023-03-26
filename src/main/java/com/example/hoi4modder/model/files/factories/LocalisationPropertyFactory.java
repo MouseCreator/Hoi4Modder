4bf1578689d34daecf87db77b7fa88d0b9ae0e63
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-26 15:07:57 +0300

    Implemented localisation property factory

--- src/main/java/com/example/hoi4modder/model/files/factories/LocalisationPropertyFactory.java
+++ src/main/java/com/example/hoi4modder/model/files/factories/LocalisationPropertyFactory.java
@@ -2,19 +2,25 @@ package com.example.hoi4modder.model.files.factories;
 
 import com.example.hoi4modder.model.files.properties.LocalisationProperty;
 import com.example.hoi4modder.model.files.properties.Property;
-import com.example.hoi4modder.model.files.properties.SingleValue;
 
 class LocalisationPropertyFactory extends AbstractFactory{
 
     private final LocalisationProperty property = new LocalisationProperty();
+
+    private final SingleValueFactory next = new SingleValueFactory();
     @Override
     public ChainedFactory next() {
-        return null;
+        return next;
     }
 
     @Override
     public Property toProperty(String origin) {
-        return null;
+        String key;
+        String value;
+        int splitPosition = origin.indexOf(":");
+        key = origin.substring(0, splitPosition);
+        value = origin.substring(splitPosition+1);
+        return new LocalisationProperty(key, value);
     }
 
     @Override
