d93157b50bbe4245f6dce9326e2b498669f12766
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-27 22:00:47 +0300

    Implemented simple style

--- src/main/java/com/example/hoi4modder/model/files/properties/styles/SimpleStyle.java
+++ src/main/java/com/example/hoi4modder/model/files/properties/styles/SimpleStyle.java
@@ -1,11 +1,18 @@
 package com.example.hoi4modder.model.files.properties.styles;
 
 import com.example.hoi4modder.model.files.properties.Block;
+import com.example.hoi4modder.model.files.properties.Property;
 
 public class SimpleStyle implements PrintStyle{
 
     @Override
-    public String styledList(Block name) {
-        return null;
+    public String styledList(Block block) {
+        StringBuilder builder = new StringBuilder(block.prefix());
+        builder.append(" ");
+        for (Property property : block.getElements()) {
+            builder.append(property).append(" ");
+        }
+        builder.append(block.suffix());
+        return builder.toString();
     }
 }
