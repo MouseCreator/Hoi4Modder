60392c0743d8bf7b8ce31c2d2da69df993480880
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-29 05:10:57 +0300

    Added unparser for portraits

--- src/main/java/com/example/hoi4modder/model/files/iovisitor/Unparser.java
+++ src/main/java/com/example/hoi4modder/model/files/iovisitor/Unparser.java
@@ -1,12 +1,43 @@
 package com.example.hoi4modder.model.files.iovisitor;
 
+import com.example.hoi4modder.game.FieldValueMap;
+import com.example.hoi4modder.game.GameCharacter;
 import com.example.hoi4modder.game.GameCharacterList;
+import com.example.hoi4modder.game.Role;
+import com.example.hoi4modder.model.files.properties.BlockProperty;
+import com.example.hoi4modder.model.files.properties.FieldValueProperty;
 import com.example.hoi4modder.model.files.properties.Property;
 
 public class Unparser implements Visitor {
 
     @Override
     public void visitCharacterList(GameCharacterList characterList, Property mainBlock) {
+       // for (GameCharacter character : characterList) {
+          //  mainBlock.add(propertyFromCharacter(character));
+       // }
+    }
 
+    private Property propertyFromCharacter(GameCharacter character) {
+        BlockProperty blockProperty = new BlockProperty();
+        blockProperty.setKey(character.getIdentification());
+        blockProperty.add(new FieldValueProperty("name", character.getName()));
+        blockProperty.add(addPortraitProperty(character.getPortraits()));
+        addRoles(character, blockProperty);
+        return null;
+    }
+
+    private void addRoles(GameCharacter character, BlockProperty mainProperty) {
+
+    }
+
+    private BlockProperty addPortraitProperty(FieldValueMap<String> portraits) {
+        BlockProperty mainBlock = new BlockProperty();
+        mainBlock.setKey("portraits");
+        BlockProperty civilianBlock = new BlockProperty();
+        civilianBlock.setKey("civilian");
+        for (String key : portraits.fields()) {
+            civilianBlock.add(new FieldValueProperty(key, portraits.get(key)));
+        }
+        return mainBlock;
     }
 }
