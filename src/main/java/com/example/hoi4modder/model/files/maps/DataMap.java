96545598bcdda4800f5918fc3319326af9815435
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-31 06:57:59 +0300

    Added mapping methods for data pool

--- src/main/java/com/example/hoi4modder/model/files/maps/DataMap.java
+++ src/main/java/com/example/hoi4modder/model/files/maps/DataMap.java
@@ -3,9 +3,41 @@ package com.example.hoi4modder.model.files.maps;
 import java.util.Map;
 
 public class DataMap {
-    private Map<String, String> map;
+    private final Map<String, String> map;
+
+    private final String filename;
+
+    public DataMap(Map<String, String> map, String filename) {
+        this.map = map;
+        this.filename = filename;
+    }
 
     public String get(String key) {
         return map.get(key);
+    }
+
+    public String put(String key, String value) {
+        return map.put(key, value);
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public boolean remove(String key) {
+        if (map.containsKey(key)) {
+            map.remove(key);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean replaceKey(String oldKey, String newKey) {
+        if (map.containsKey(oldKey)) {
+            String value = map.remove(oldKey);
+            map.put(newKey, value);
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/example/hoi4modder/model/files/maps/DataPool.java
+++ src/main/java/com/example/hoi4modder/model/files/maps/DataPool.java
@@ -1,10 +1,15 @@
 package com.example.hoi4modder.model.files.maps;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class DataPool {
-    private Map<String, DataMap> maps;
+    private final Map<String, DataMap> maps;
+
+    public DataPool(Map<String, DataMap> maps) {
+        this.maps = maps;
+    }
 
     public String get(String key) {
         for (String current : maps.keySet()) {
@@ -14,4 +19,32 @@ public class DataPool {
         }
         throw new NoSuchElementException("Cannot find value " + key + " in the data pool");
     }
+
+    public String put(String type, String key, String value) {
+        DataMap mapToAdd = maps.get(type);
+        return mapToAdd.put(key, value);
+    }
+
+    public boolean createMapType(String type, String filename) {
+        if (maps.containsKey(type)) {
+            return false;
+        }
+        maps.put(type, new DataMap(new HashMap<>(), filename));
+        return true;
+    }
+    public boolean remove(String key) {
+        for (String current : maps.keySet()) {
+            if(maps.get(current).remove(key))
+                return true;
+        }
+        return false;
+    }
+    public boolean replace(String oldKey, String newKey) {
+        for (String current : maps.keySet()) {
+            if(maps.get(current).replaceKey(oldKey,newKey))
+                return true;
+        }
+        return false;
+    }
+
 }
