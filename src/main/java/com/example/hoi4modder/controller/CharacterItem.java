46d6662473cb93ea4982e710324e7c0675cf59c2
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-04-01 23:31:24 +0300

    Added auto button for small portrait

--- src/main/java/com/example/hoi4modder/controller/CharacterItem.java
+++ src/main/java/com/example/hoi4modder/controller/CharacterItem.java
@@ -1,4 +1,37 @@
 package com.example.hoi4modder.controller;
 
-public class CharacterItem {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CharacterItem implements Initializable {
+    @FXML
+    private CheckBox advisorBox;
+
+    @FXML
+    private Button autoSmallPortrait;
+
+    @FXML
+    private TextField characterIDField;
+
+    @FXML
+    private TextField characterNameField;
+
+    @FXML
+    private CheckBox countryLeaderBox;
+
+    @FXML
+    private CheckBox navyLeaderBox;
+
+    @FXML
+    private CheckBox unitLeaderBox;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
