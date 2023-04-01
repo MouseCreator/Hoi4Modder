563661c16ec0e1541100b50ae3836e213c164d43
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-04-01 16:52:23 +0300

    Added character list editor

---
+++ src/main/java/com/example/hoi4modder/controller/CharacterListEditor.java
@@ -0,0 +1,16 @@
+package com.example.hoi4modder.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class CharacterListEditor {
+
+    @FXML
+    private ListView<?> charactersListView;
+
+    @FXML
+    private TextField tagTextField;
+
+}
+
