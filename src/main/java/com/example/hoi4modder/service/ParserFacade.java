acb9f7c215bcd92f9c9b1f723bc5ad423306eed8
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-04-02 15:31:41 +0300

    Refactor: visitable in parser facade

--- src/main/java/com/example/hoi4modder/service/ParserFacade.java
+++ src/main/java/com/example/hoi4modder/service/ParserFacade.java
@@ -3,6 +3,7 @@ package com.example.hoi4modder.service;
 import com.example.hoi4modder.game.GameCharacterList;
 import com.example.hoi4modder.game.collection.LocalisationMap;
 import com.example.hoi4modder.game.collection.SpriteList;
+import com.example.hoi4modder.game.collection.Visitable;
 import com.example.hoi4modder.model.files.StringToPropertyConvertor;
 import com.example.hoi4modder.model.files.iovisitor.Parser;
 import com.example.hoi4modder.model.files.manager.GameFilesReader;
@@ -11,35 +12,29 @@ import java.io.IOException;
 
 public class ParserFacade {
     public GameCharacterList characterListFromFile(String filename) throws IOException {
-        GameFilesReader reader = new GameFilesReader();
-        StringToPropertyConvertor convertor = new StringToPropertyConvertor();
-        String characters = reader.read(filename);
-        Parser parser = new Parser();
-        parser.setBlock(convertor.forStructuredFile(characters));
         GameCharacterList list = GameCharacterList.getArrayList();
-        parser.visitCharacterList(list);
+        parseVisitable(filename, list);
         return list;
     }
 
-    public SpriteList spriteListFromFile(String filename) throws IOException {
+    private void parseVisitable(String filename, Visitable visitable) throws IOException {
         GameFilesReader reader = new GameFilesReader();
         StringToPropertyConvertor convertor = new StringToPropertyConvertor();
         String characters = reader.read(filename);
         Parser parser = new Parser();
         parser.setBlock(convertor.forStructuredFile(characters));
+        visitable.acceptVisitor(parser);
+    }
+
+    public SpriteList spriteListFromFile(String filename) throws IOException {
         SpriteList list = new SpriteList();
-        parser.visitSpriteList(list);
+        parseVisitable(filename, list);
         return list;
     }
 
     public LocalisationMap localisationMapFromFile(String filename) throws IOException {
-        GameFilesReader reader = new GameFilesReader();
-        StringToPropertyConvertor convertor = new StringToPropertyConvertor();
-        String localisation = reader.read(filename);
-        Parser parser = new Parser();
-        parser.setBlock(convertor.forStructuredFile(localisation));
         LocalisationMap localisationMap = new LocalisationMap();
-        parser.visitLocalisationMap(localisationMap);
+       parseVisitable(filename, localisationMap);
         return localisationMap;
     }
 }
