e2d61405202f6c3f8a988be0032320c509c6a0db
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-04-02 17:30:53 +0300

    Added property documentation

--- src/main/java/com/example/hoi4modder/model/files/properties/Property.java
+++ src/main/java/com/example/hoi4modder/model/files/properties/Property.java
@@ -2,17 +2,72 @@ package com.example.hoi4modder.model.files.properties;
 
 import com.example.hoi4modder.model.files.properties.lists.PropertyCollection;
 
+/**
+ * COMPOSITE PATTERN
+ */
 public interface Property {
+    /**
+     *
+     * @return string that separates name and value of the property
+     */
     String delimiter();
-    boolean containsDelimiter(String str);
-    String toFile();
-    String name();
-    String value();
-    void add(Property other);
-    void put(Property other);
 
-    PropertyCollection getAll();
+    /**
+     *
+     * @param str - string from formatted file
+     * @return true, if string fits property signature
+     */
+    boolean containsDelimiter(String str);
+
+    /**
+     * Turns property to string in format for file
+     * @return string in format for file
+     */
+    String toFile();
+
+    /**
+     * @return name of the property
+     */
+    String name();
+
+    /**
+     *
+     * @return value of the property
+     */
+    String value();
+
+    /**
+     * Adds property if this property is a block
+     * @param other - property to add in the block
+     * @throws UnsupportedOperationException if property is simple
+     */
+    void add(Property other) throws UnsupportedOperationException;
+
+    /**
+     * Adds property to block if the block doesn't contain it
+     * @param other - property to put
+     * @throws UnsupportedOperationException if property is simple
+     */
+    void put(Property other) throws UnsupportedOperationException;
+
+    /**
+     *
+     * @return list of all child properties of the block
+     * @throws UnsupportedOperationException if property is simple
+     */
+    PropertyCollection getAll() throws UnsupportedOperationException;
+
+    /**
+     * Gets properties by name. Method is used to get properties that may have multiple occurrences
+     * @param field - name of target property
+     * @return list of all properties with name {@param field}
+     */
     PropertyCollection get(String field);
 
+    /**
+     * Gets one of the properties by name. Method is used to get properties that usually have only one occurrence
+     * @param field - name of target property
+     * @return first instance of the property named {@param field}
+     */
     Property getFirst(String field);
 }
--- src/main/java/com/example/hoi4modder/model/files/properties/SingleValue.java
+++ src/main/java/com/example/hoi4modder/model/files/properties/SingleValue.java
@@ -2,36 +2,69 @@ package com.example.hoi4modder.model.files.properties;
 
 import com.example.hoi4modder.model.files.properties.lists.PropertyCollection;
 
+/**
+ * Property that is represented by a single word or number.
+ * May be separated with others with space or newline
+ */
 public class SingleValue implements Property{
     private final String value;
 
+    /**
+     *
+     * @param origin - word or number to put in property
+     */
     public SingleValue(String origin) {
         this.value = origin;
     }
+
+    /**
+     * Creates empty property
+     */
     public SingleValue() {
         this.value = "";
     }
 
+    /**
+     *
+     * @return delimiter of single value - empty string, since it contains only value
+     */
     @Override
     public String delimiter() {
         return "";
     }
 
+    /**
+     *
+     * @param str - string from formatted file
+     * @return always true, since any string may be considered as set of words
+     */
     @Override
     public boolean containsDelimiter(String str) {
         return true;
     }
 
+    /**
+     *
+     * @return value of the property
+     */
     @Override
     public String toFile() {
         return value;
     }
 
+    /**
+     *
+     * @return empty string, since single value contains unnamed value
+     */
     @Override
     public String name() {
         return "";
     }
 
+    /**
+     *
+     * @return value of the property
+     */
     @Override
     public String value() {
         return value;
