bc69778ab4737112640be9319b256b31d1d8f335
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-04-29 19:03:36 +0300

    Added methods in builder for other roles

--- src/main/java/com/example/hoi4modder/controller/character_extra/RoleSwitcherBuilder.java
+++ src/main/java/com/example/hoi4modder/controller/character_extra/RoleSwitcherBuilder.java
@@ -2,14 +2,41 @@ package com.example.hoi4modder.controller.character_extra;
 
 import com.example.hoi4modder.controller.CharacterItemController;
 import com.example.hoi4modder.game.roles.Advisor;
+import com.example.hoi4modder.game.roles.CountryLeader;
+import com.example.hoi4modder.game.roles.NavyLeader;
+import com.example.hoi4modder.game.roles.UnitLeader;
 
 public class RoleSwitcherBuilder {
-    public RoleSwitcher<Advisor> buildAdvisor(CharacterItemController itemController) {
+    public RoleSwitcher<Advisor> buildAdvisorSwitcher(CharacterItemController itemController) {
         RoleSwitcher<Advisor> roleSwitcher = new RoleSwitcher<>(itemController);
         roleSwitcher.setRolesBox(itemController.getRolesBox());
-        roleSwitcher.setTargetIndex(3);
         roleSwitcher.setPaneController(new RolePaneControllerPair<>());
+        roleSwitcher.setTargetIndex(3);
         roleSwitcher.bindCheckBox(itemController.getAdvisorCheckBox());
+        return roleSwitcher;
+    }
+    public RoleSwitcher<CountryLeader> buildCountryLeaderSwitcher(CharacterItemController itemController) {
+        RoleSwitcher<CountryLeader> roleSwitcher = new RoleSwitcher<>(itemController);
+        roleSwitcher.setRolesBox(itemController.getRolesBox());
+        roleSwitcher.setPaneController(new RolePaneControllerPair<>());
+        roleSwitcher.setTargetIndex(0);
+        roleSwitcher.bindCheckBox(itemController.getCountryLeaderBox());
+        return roleSwitcher;
+    }
+    public RoleSwitcher<UnitLeader> buildUnitLeaderSwitcher(CharacterItemController itemController) {
+        RoleSwitcher<UnitLeader> roleSwitcher = new RoleSwitcher<>(itemController);
+        roleSwitcher.setRolesBox(itemController.getRolesBox());
+        roleSwitcher.setPaneController(new RolePaneControllerPair<>());
+        roleSwitcher.setTargetIndex(1);
+        roleSwitcher.bindCheckBox(itemController.getUnitLeaderBox());
+        return roleSwitcher;
+    }
+    public RoleSwitcher<NavyLeader> buildNavyLeaderSwitcher(CharacterItemController itemController) {
+        RoleSwitcher<NavyLeader> roleSwitcher = new RoleSwitcher<>(itemController);
+        roleSwitcher.setRolesBox(itemController.getRolesBox());
+        roleSwitcher.setPaneController(new RolePaneControllerPair<>());
+        roleSwitcher.setTargetIndex(2);
+        roleSwitcher.bindCheckBox(itemController.getNavyLeaderBox());
         return roleSwitcher;
     }
 }
