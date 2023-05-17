b67e5d6d38e6f6b1e3ad2c29315992177985e1e2
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-05-18 00:29:24 +0300

    Added callable support for ignored action runner

--- src/main/java/com/example/hoi4modder/controller/command/roles/IgnoredActionRunner.java
+++ src/main/java/com/example/hoi4modder/controller/command/roles/IgnoredActionRunner.java
@@ -1,5 +1,8 @@
 package com.example.hoi4modder.controller.command.roles;
 
+
+import java.util.concurrent.Callable;
+
 public class IgnoredActionRunner {
     public void runAction(UndoRedoManager undoRedoManager, Runnable runnable) {
         if (undoRedoManager == null) {
@@ -10,5 +13,15 @@ public class IgnoredActionRunner {
         undoRedoManager.disableAll();
         runnable.run();
         undoRedoManager.lastState();
+    }
+    public <V> V callAction(UndoRedoManager undoRedoManager, Callable<V> callable) throws Exception {
+        if (undoRedoManager == null) {
+            return callable.call();
+        }
+        undoRedoManager.saveState();
+        undoRedoManager.disableAll();
+        V result = callable.call();
+        undoRedoManager.lastState();
+        return result;
     }
 }
